d7b042c33c77add21e03938ca973796407580acc
Henry Parrish <henryhparrish@gmail.com>
2020-09-27 14:56:21 -0500

    Updated Db

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,3 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, devoured) VALUES ('Sylvester', true);
-INSERT INTO cats (name, devoured) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, devoured) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Double Beef Burger', false);
