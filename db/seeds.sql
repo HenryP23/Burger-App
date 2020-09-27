f5301cb1bed5ff42c5dde7b76c55b2f4cc63650b
Henry Parrish <henryhparrish@gmail.com>
2020-09-27 14:34:23 -0500

    make it better

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO cats (name) VALUES ('Meeses');
+INSERT INTO cats (name) VALUES ('Bobbi');
+INSERT INTO cats (name, devoured) VALUES ('Sylvester', true);
+INSERT INTO cats (name, devoured) VALUES ('Marilyn Manson', true);
+INSERT INTO cats (name, devoured) VALUES ('Joe Biden', true);
+INSERT INTO cats (name) VALUES ('Owens');
